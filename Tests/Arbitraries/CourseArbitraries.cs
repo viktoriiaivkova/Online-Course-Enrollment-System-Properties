621232c01e96c7e11078fbb6b279a7df7ab1135d
viktoriiaivkova <jesuspristine@gmail.com>
2025-12-19 22:08:32 +0200

    Added tests.

--- Tests/Arbitraries/CourseArbitraries.cs
+++ Tests/Arbitraries/CourseArbitraries.cs
@@ -1,27 +1,30 @@
 ﻿using FsCheck;
-using FsCheck.Fluent;
 using Online_Course_Enrollment_System_Properties.Domain;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Online_Course_Enrollment_System_Properties.Tests.Arbitraries
 {
     public static class CourseArbitraries
     {
+        public static Arbitrary<string> NonEmptyString() =>
+            Arb.Default.String().Filter(s => !string.IsNullOrWhiteSpace(s));
+
+        public static Arbitrary<int> PositiveInt() =>
+            Arb.Default.Int32().Filter(i => i > 0 && i <= 1000);
+
         public static Arbitrary<Student> Student()
         {
-            throw new NotImplementedException();
+            var gen = NonEmptyString().Generator.Select(id => new Student(id));
+            return Arb.From(gen);
         }
 
         public static Arbitrary<Course> Course()
         {
-            throw new NotImplementedException();
-
+            var gen = from id in NonEmptyString().Generator
+                      from capacity in Gen.Choose(1, 100)
+                      select new Course(id, capacity);
+            return Arb.From(gen);
         }
     }
-
 }
--- Tests/Properties/EnrollmentProperties.cs
+++ Tests/Properties/EnrollmentProperties.cs
@@ -3,13 +3,116 @@ using FsCheck.Xunit;
 using Online_Course_Enrollment_System_Properties.Domain;
 using Online_Course_Enrollment_System_Properties.Tests.Arbitraries;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace Online_Course_Enrollment_System_Properties.Tests.Properties
 {
     public class EnrollmentProperties
     {
-        
-    }
+        private readonly EnrollmentRepository _repository = new();
 
+        [Property(Arbitrary = new[] { typeof(CourseArbitraries) })]
+        public bool NewCourse_IsAlways_Empty(Course course)
+        {
+            return course.Students.Count == 0;
+        }
+
+        [Property(Arbitrary = new[] { typeof(CourseArbitraries) })]
+        public bool Student_CannotBeEnrolled_Twice(Course course, Student student)
+        {
+            course.Enroll(student);
+            var secondEnrollment = course.Enroll(student);
+            return secondEnrollment == false && course.Students.Count(s => s.Id == student.Id) == 1;
+        }
+
+        [Property(Arbitrary = new[] { typeof(CourseArbitraries) })]
+        public bool Course_CannotExceed_Capacity(Course course, List<Student> students)
+        {
+            foreach (var s in students) course.Enroll(s);
+            return course.Students.Count <= course.Capacity;
+        }
+
+        [Property(Arbitrary = new[] { typeof(CourseArbitraries) })]
+        public bool Enrollment_ReturnsFalse_WhenCourseIsFull(Student s1, Student s2)
+        {
+            var course = new Course("ValidCourse", 1);
+            course.Enroll(s1);
+            if (s1.Id == s2.Id) return true;
+            return course.Enroll(s2) == false;
+        }
+
+        [Property(Arbitrary = new[] { typeof(CourseArbitraries) })]
+        public bool Operations_OverValidData_NeverThrowExceptions(Course course, Student student)
+        {
+            var exception = Record.Exception(() => course.Enroll(student));
+            return exception == null;
+        }
+
+        [Property(Arbitrary = new[] { typeof(CourseArbitraries) })]
+        public bool Repository_SavesAndRetrieves_WithoutLoss(Course course)
+        {
+            _repository.Save(course);
+            return _repository.All().Any(c => c.Id == course.Id);
+        }
+
+        [Property(Arbitrary = new[] { typeof(CourseArbitraries) })]
+        public Property Enrolling_UniqueStudents_UpToCapacity_AlwaysSucceeds()
+        {
+            return Prop.ForAll(CourseArbitraries.NonEmptyString(), CourseArbitraries.PositiveInt(), (id, capacity) =>
+            {
+                var course = new Course(id, capacity);
+                var results = new List<bool>();
+                for (int i = 0; i < capacity; i++)
+                {
+                    results.Add(course.Enroll(new Student($"Unique-{i}")));
+                }
+                return (results.All(r => r == true) && course.Students.Count == capacity).ToProperty();
+            });
+        }
+
+        [Property(Arbitrary = new[] { typeof(CourseArbitraries) })]
+        public bool StudentsCollection_IsConsistentWithEnrollment(Course course, Student student)
+        {
+            var countBefore = course.Students.Count;
+            var added = course.Enroll(student);
+            var expectedCount = added ? countBefore + 1 : countBefore;
+
+            return course.Students.Count == expectedCount;
+        }
+
+        [Property(Arbitrary = new[] { typeof(CourseArbitraries) })]
+        public bool Repository_Consistency_MatchesOperations(List<Course> courses)
+        {
+            var repo = new EnrollmentRepository();
+            foreach (var c in courses) repo.Save(c);
+            return repo.All().Count == courses.Count;
+        }
+
+        [Property(Arbitrary = new[] { typeof(CourseArbitraries) })]
+        public bool Enrollment_HasNoSideEffects_OnOtherCourses(Course c1, Course c2, Student s)
+        {
+            if (c1.Id == c2.Id) return true;
+            int initialCount2 = c2.Students.Count;
+            c1.Enroll(s);
+            return c2.Students.Count == initialCount2;
+        }
+
+        [Property(Arbitrary = new[] { typeof(CourseArbitraries) })]
+        public bool StudentId_RemainsStable(Course course, Student student)
+        {
+            course.Enroll(student);
+            return course.Students.Any(s => s.Id == student.Id);
+        }
+
+        [Property(Arbitrary = new[] { typeof(CourseArbitraries) })]
+        public bool CourseProperties_AreImmutable(Course course, Student student)
+        {
+            var id = course.Id;
+            var cap = course.Capacity;
+            course.Enroll(student);
+            return course.Id == id && course.Capacity == cap;
+        }
+    }
 }
